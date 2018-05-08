bfabe4800f60f56ff6d92eeb6c214d9ab9fd07f9
Dovydas Tamulis <dovydastt@gmail.com>
2018-05-08 11:37:47 +0200

    ngClass responds to router.url

--- src/app/home-router/lol/lol.component.ts
+++ src/app/home-router/lol/lol.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-lol',
@@ -8,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 export class LolComponent implements OnInit {
 
 
-  constructor() { }
+  constructor(public router:Router) { }
 
   ngOnInit() {
   }
